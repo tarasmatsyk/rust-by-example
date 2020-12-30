b0a2c9d59bfe87f8140c3bf0f7c0aee6ff503e8b
Taras Matsyk <tamatsyk@gmail.com>
2020-12-30 23:49:06 +0200

    Scope experiments

--- variables_binding/src/main.rs
+++ variables_binding/src/main.rs
@@ -17,4 +17,20 @@ fn main() {
     let _noisy_unused_variable = 2u32;
     println!("{:?}", _noisy_unused_variable);
     // FIXME ^ Prefix with an underscore to suppress the warning
+
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding);
 }
